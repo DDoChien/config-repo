1cf2c2d81ff468605a12bb11b00ba02dbc888c7d
chien <john.doe@example.com>
2024-06-01 11:26:29 +0700

    Initial commit

---
+++ src/main/java/com/example/demo_springcloud/DemoSpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringcloudApplication.class, args);
+	}
+
+}
